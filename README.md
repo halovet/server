fcfe6fa9e217fb2edf1f29cba1729687ea61f0b0
unknown <salwa21salsabila@gmail.com>
2023-07-05 22:40:25 +0800

    feat: register and login user

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # API Documentation
 
+List of available endpoints:
+
+- `POST /userRegister`
+- `POST /userLogin`
+
+&nbsp;
+
+## 1. POST /userRegister
+
+Description:
+- Register user
+
+Request:
+
+- body:
+
+```json
+{
+  "username": "string",
+  "email": "string (unique)" ,
+  "password": "string"
+}
+```
+
+_Response (201 - Created)_
+
+```json
+{
+  "message": "user with email user1@mail.com create successfully"
+}
+
+```
+
+&nbsp;
+
+## 2. POST /userLogin
+
+Description:
+- Login an user
+
+Request:
+
+- body:
+
+```json
+{
+  "email": "string",
+  "password": "string"
+}
+```
+
+_Response (200 - OK)_
+
+```json
+{
+  "username": "string",
+  "access_token": "string"
+}
+
+```
+
+&nbsp;
---
+++ controllers/staffController.js
@@ -0,0 +1,53 @@
+const { comparePassword, signToken } = require("../helpers/helper");
+const User = require("../model/User");
+
+module.exports = class StaffController {
+  static async userRegister(req, res, next) {
+    try {
+      const { username, email, password } = req.body;
+
+      if (!username) throw { name: "Username Empty" };
+      if (!email) throw { name: "Email Empty" };
+      if (!password) throw { name: "Password Empty" };
+
+      let isRegisterd = await User.findOne({ email });
+      if (isRegisterd) throw { name: "Email Not Unique" };
+
+      let newUser = new User({ username, email, password });
+      await newUser.save();
+
+      res.status(201).json({
+        message: `user with email ${newUser.email} create successfully`,
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ staffController.js ~ StaffController ~ userRegister:",
+        error
+      );
+      next(error);
+    }
+  }
+
+  static async userLogin(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) throw { name: "Email Empty" };
+      if (!password) throw { name: "Password Empty" };
+
+      const user = await User.findOne({ email });
+      if (!user) throw { name: "Email False" };
+
+      const isValidPassword = comparePassword(password, user.password);
+      if (!isValidPassword) throw { name: "Password False" };
+
+      const payload = { id: user._id };
+      const token = signToken(payload);
+
+      res.status(200).json({ username: user.username, access_token: token });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+};
